7c16f84c6264d4491dd78753d967c149cfb7b9e0
Lekhak2277 <lekhakgowda1177@gmail.com>
2026-03-04 23:23:58 +0530

    pushing the correct code 71A

--- Codeforces/Way Too Long Words 71A.py
+++ Codeforces/Way Too Long Words 71A.py
@@ -39,3 +39,13 @@ for i in ar:
         print(ful_str)
 
 
+#CORRECT CODE 
+
+n=int(input())
+for i in range(n):
+    word=input().strip()
+    if len(word)<=10:
+        print(word)
+    else:
+        print(word[0]+str(len(word)-2)+word[-1])
+
