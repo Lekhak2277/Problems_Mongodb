b759131355542927b8db966e4ee7cfb74f15563a
Lekhak2277 <lekhakgowda1177@gmail.com>
2026-03-21 23:10:12 +0530

    pushing the code

--- Codeforces/tempCodeRunnerFile.py
+++ Codeforces/tempCodeRunnerFile.py
@@ -1,1 +1,21 @@
-n=int(input('enter'))
+input())
+# p=input()
+# p.split()
+# q=input()
+# q.split()
+
+# new_set=set()
+
+# for i in p:
+#     new_set.add(i)
+# for i in q:
+#     new_set.add(i)
+
+# print(len(new_set))
+# print(new_set.remove(' '))
+# print(len(new_set),'after removing')
+
+# if n==len(new_set):
+#     print(True)
+# else:
+#     print(False)
